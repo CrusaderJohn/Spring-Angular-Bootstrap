fedabcddac31453e2888b26ac382591167ab04dd
John Jamieson <CrusaderJohn@JohnJamieson.ca>
2024-07-25 12:11:32 -0400

    Spring Initializr

---
+++ src/main/java/ca/johnjamieson/Spring_Angular_Bootstrap/SpringAngularBootstrapApplication.java
@@ -0,0 +1,13 @@
+package ca.johnjamieson.Spring_Angular_Bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularBootstrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/johnjamieson/Spring_Angular_Bootstrap/SpringAngularBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.johnjamieson.Spring_Angular_Bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
